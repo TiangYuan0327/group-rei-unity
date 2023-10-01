d18772e6addbf087b0b93db06bdb4317bf3b94d6
TiangYuan0327 <vincenttpe2255@gmail.com>
2023-10-01 14:51:38 +0800

    Add Ouch judge

--- Assets/Scripts/NoteObject.cs
+++ Assets/Scripts/NoteObject.cs
@@ -5,60 +5,15 @@ using UnityEngine.UI;
 
 public class NoteObject : MonoBehaviour
 {
-    /*private string jText;
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Crash!")) jText = "Crash!";
-        else if (collision.CompareTag("Smash!")) jText = "Smash!";
-        else if (collision.CompareTag("Bang!")) jText = "Bang!";
-        else if (collision.CompareTag("Boo!")) jText = "Boo!";
-        else if (collision.CompareTag("Ouch!")) jText = "Ouch!";
-
-        if (collision.CompareTag("LeftHandle") && Joystick.crash == 1)
+        if (collision.CompareTag("Ouch!"))
         {
-            if (((gameObject.CompareTag("NormalHit") || gameObject.CompareTag("ColorHit"))))
-            {
-                if (jText != null && (jText == "Crash!" || jText == "Smash!" || jText == "Bang!"))
-                {
-                    AddCombo();
-                    Destroy(gameObject);
-                }
-                else if (jText != null && (jText == "Boo!" || jText == "Ouch!"))
-                {
-                    ClearCombo();
-                    Destroy(gameObject);
-                }
-            }
+            Combo.combonumber = 0;
+            Combo.judgeText = "Ouch!";
+            Destroy(gameObject);
         }
-        else if (collision.CompareTag("RightHandle") && Joystick.crash == 2)
-        {
-            if (gameObject.CompareTag("NormalHit") || gameObject.CompareTag("ColorHit"))
-            {
-                if (jText != null && (jText == "Crash!" || jText == "Smash!" || jText == "Bang!"))
-                {
-                    AddCombo();
-                    Destroy(gameObject);
-                }
-                else if (jText != null && (jText == "Boo!" || jText == "Ouch!"))
-                {
-                    ClearCombo();
-                    Destroy(gameObject);
-                }
-            }
-        }
+        
+        
     }
-
-    private void AddCombo()
-    {
-        Combo.combonumber++;
-        Combo.judgeText = jText;
-        Debug.Log(Combo.combonumber);
-    }
-
-    private void ClearCombo()
-    {
-        Combo.combonumber = 0;
-        Combo.judgeText = jText;
-        Debug.Log(Combo.combonumber);
-    }*/
 }
