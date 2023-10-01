703493174eab8a74e535a593c7dbbed84282b291
TiangYuan0327 <vincenttpe2255@gmail.com>
2023-10-01 13:56:24 +0800

    add resolution adjustment

---
+++ Assets/Scripts/Resolution.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resolution : MonoBehaviour
+{
+    public float referenceScreenWidth = 1800f; 
+    public float referenceScreenHeight = 900f; 
+
+    private void Start()
+    {
+        float screenRatio = Screen.width / (float)Screen.height;
+        float referenceRatio = referenceScreenWidth / referenceScreenHeight;
+        Debug.Log(screenRatio);
+        Debug.Log(referenceRatio);
+
+        Camera mainCamera = Camera.main;
+        mainCamera.orthographicSize *= referenceRatio / screenRatio;
+        Debug.Log(mainCamera.orthographicSize);
+    }
+}
