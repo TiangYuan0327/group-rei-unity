33752c717999bd970d27abebb55dae021e92a6dd
TiangYuan0327 <vincenttpe2255@gmail.com>
2023-10-12 12:00:08 +0800

    NoteRead(Have Bug)

--- Assets/Scripts/CreatNote.cs
+++ Assets/Scripts/CreatNote.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using System.Xml.Serialization;
 using System;
 using System.IO;
-using UnityEditor.Experimental.RestService;
+
 
 public class CreatNote : MonoBehaviour
 {
@@ -12,43 +12,54 @@ public class CreatNote : MonoBehaviour
     public GameObject note;
     public Transform canvasTransform;
 
+    public string fileName = "/NoteTest.json";
+    public NoteData[] songs;
+
     [SerializeField]
     NoteData notedata;
 
     [System.Serializable]
     public class NoteData
     {
-        public string noteText;
-        public int notenumber;
+        //音符種類、出現時機、音符長度、出現位置
+        public string noteType;
+        public float noteTime;
+        public float noteHeight;
+        public int noteLocation;
     }
     private void Start()
     {
-        FileStream fs = new FileStream(Application.dataPath + "/SaveTest.txt", FileMode.Open);
-        StreamReader sr = new StreamReader(fs);
-        notedata.noteText = sr.ReadLine();
-        notedata.notenumber = int.Parse(sr.ReadLine());
+            
     }
-
+    
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.S))
         {
-            PlayerPrefs.SetInt("notenumber", notedata.notenumber);
-            FileStream fs = new FileStream(Application.dataPath + "/SaveTest.txt", FileMode.Create);
-            StreamWriter sw = new StreamWriter(fs);
-            sw.WriteLine(notedata.noteText);
-            sw.WriteLine(notedata.notenumber);        
-            sw.Close();
-            fs.Close();
+            Debug.Log(Application.dataPath);
+            string filePath = Application.dataPath + "/NoteTest.json";
+            if (File.Exists(filePath)) {
+                string json = File.ReadAllText(filePath);
+                songs = JsonUtility.FromJson<NoteData[]>(json);
+
+                foreach (NoteData song in songs)
+                {
+                    Debug.Log("Type: " + song.noteType);
+                    Debug.Log("Time: " + song.noteTime);
+                    Debug.Log("長度: " + song.noteHeight);
+                    Debug.Log("位置" + song.noteLocation);
+                }
+            }
+            else
+            {
+                Debug.Log("not found");
+            }
+
             Debug.Log("有喔");
         }       
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (notedata.notenumber == 1)
-            {
-                GameObject createNote = Instantiate(note, new Vector2(0,0), Quaternion.identity);
-                createNote.transform.SetParent(canvasTransform, false);
-            }
+            
         }
     }
 }
--- Assets/Scripts/Fall.cs
+++ Assets/Scripts/Fall.cs
@@ -7,14 +7,11 @@ public class Fall : MonoBehaviour
     public float beatTample;
     public bool hasStart;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         beatTample = beatTample / 60f;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.anyKeyDown)
@@ -24,7 +21,6 @@ public class Fall : MonoBehaviour
         else
         {
             transform.position -= new Vector3(0f, beatTample * Time.deltaTime, 0f);
-            //transform.localScale += new Vector3(beatTample / 1800, beatTample / 1800, beatTample / 1800);
         }
     }
 }
