5c8d7d9d64428365726b27e68b3e93c3c4b29867
ycc2001 <ycc2001@gmail.com>
2023-12-17 17:19:40 +0800

    build login & signup System
    create 3 scenes (StartPage, Login, Signup), 3 scripts(StartPage, SignupRequest, LoginRequest)

---
+++ Assets/Scripts/LoginRequest.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+using UnityEngine.UI;
+using System.Net.Http;
+using UnityEngine.SceneManagement;
+
+
+
+public class LoginRequest : MonoBehaviour
+{
+    InputField outputArea;
+    public InputField account;
+    public InputField password;
+    // Start is called before the first frame update
+    void Start()
+    {
+        outputArea = GameObject.Find("OutputArea").GetComponent<InputField>();
+        GameObject.Find("Button").GetComponent<Button>().onClick.AddListener(PostData);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    async void PostData()
+    {
+        StartCoroutine(PostRequest());
+
+    }
+
+    IEnumerator PostRequest()
+    {
+        string url = "http://127.0.0.1:5000/account/login";
+        string jsonData = "{\"uid\": \"" + account.text + "\", \"password\": \"" + password.text + "\" }";
+        byte[] postData = System.Text.Encoding.UTF8.GetBytes(jsonData);
+
+        using (UnityWebRequest request = UnityWebRequest.Post(url, "Post"))
+        {
+            request.uploadHandler = new UploadHandlerRaw(postData);
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                outputArea.text = request.error;
+            }
+            else
+            {
+                outputArea.text = request.downloadHandler.text;
+            }
+        }
+    }
+    public void back()
+    {
+        SceneManager.LoadScene("StartPage");
+
+
+
+    }
+}
